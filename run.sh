77bda658cbbdfa2988eae292261a5c68a231b9f9
Ivan Firmansyah <62599033+F713ND5@users.noreply.github.com>
2020-04-05 09:52:19 +0700

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,3 +1,5 @@
+#apa lu liat liat?
+#mau recode? Gabisa eaa
 z="
 ";Rz='Auth';nz='reco';Mz='════';ez='chan';Qz='7m  ';Gz='figl';Vz=' Fir';Oz='\033';Ez=';32m';cz='anne';iz='y Re';Wz='mans';Cz=' "\0';Pz='[1;3';Kz=';31m';qz='ti';Bz='echo';Dz='33[1';Jz='er';Zz='page';pz='u_ma';hz='Lerr';Fz='"';az='Blac';Iz='pamm';Nz='══╕';Uz='Ivan';Az='lear';bz='kSc';Sz='or  ';Xz='yah';Yz='Fans';lz='╛';Tz='  : ';jz='al';dz='r';kz='1m╘═';gz='t : ';Lz='╒═══';mz='php ';Hz='et S';oz='de_l';fz='el y';
 eval "$Az$z$Bz$Cz$Dz$Ez$Fz$z$Gz$Hz$Iz$Jz$z$Bz$Cz$Dz$Kz$Lz$Mz$Mz$Mz$Mz$Mz$Mz$Mz$Nz$z$Oz$Pz$Qz$Rz$Sz$Tz$Uz$Vz$Wz$Xz$z$Yz$Zz$Tz$az$bz$cz$dz$z$ez$fz$gz$hz$iz$jz$z$Oz$Pz$kz$Mz$Mz$Mz$Mz$Mz$Mz$Mz$Mz$lz$z$Fz$z$mz$nz$oz$pz$qz"
